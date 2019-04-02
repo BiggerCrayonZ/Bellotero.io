4b2807343ddfc08807f7543dd0176cec443d6d19
Raúl Enrique Reza del Castillo <32489142+BiggerCrayonZ@users.noreply.github.com>
2019-04-02 13:25:28 -0600

    init theme-colors

--- bellotero-ui/src/App.js
+++ bellotero-ui/src/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { connect } from 'react-redux';
+
+// Actions
+import globalActions from './redux/actions/globalActions';
+
+// Router
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
 
+  // React LC
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(globalActions.getGlobalData());
+  }
+
   render() {
-    console.log('props: ', this.props)
+    // console.log('render: ', this.props)
     return (
-      <div className="App">
-        Init Bellotero
-      </div>
+      <Router>
+        <div className="App w-100">
+          <div className="bg-white d-flex justify-content-around">
+            <div className="d-flex justify-content-center align-items-center">
+              Logotipo
+            </div>
+          </div>
+          <div className="bg-info">
+
+          </div>
+        </div>
+      </Router>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  ...state
-})
-
-export default connect(mapStateToProps)(App)
-
-// export default App;
+export default App;
