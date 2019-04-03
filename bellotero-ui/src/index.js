6f1873edc57325d9d8fc0c5c4b7df249a7c499d7
Raúl Enrique Reza del Castillo <32489142+BiggerCrayonZ@users.noreply.github.com>
2019-04-02 23:26:18 -0600

    Redux DevTools Extension added

--- bellotero-ui/src/index.js
+++ bellotero-ui/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Redux Provider
-import { Provider } from 'react-redux'
-import configureStore from './redux/store/store';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+import reducers from './redux/reducers';
+
 
 import './index.scss';
 import App from './AppContainer';
 
+let createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+let store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 ReactDOM.render(
-    <Provider store={configureStore()}>
+    <Provider store={store}>
         <App />
     </Provider>
     , document.getElementById('root'));
