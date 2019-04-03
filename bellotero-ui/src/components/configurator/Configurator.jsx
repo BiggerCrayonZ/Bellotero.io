ab784c453b84037211403e7afe3179ee93122793
Raúl Enrique Reza del Castillo <32489142+BiggerCrayonZ@users.noreply.github.com>
2019-04-03 04:02:05 -0600

    Configurator template init

--- bellotero-ui/src/components/configurator/Configurator.jsx
+++ bellotero-ui/src/components/configurator/Configurator.jsx
@@ -4,13 +4,21 @@ import React, { Component } from 'react';
 import './Configurator.scss';
 
 class Configurator extends Component {
-  render() {
-    return (
-      <div className="configurator_container d-flex w-100">
-        Configurator
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="configurator_container w-100 d-flex justify-content-between ">
+                <div className="info_section d-flex flex-column w-100">
+                    <h4 className="conf_header bg-primary text-white px-2 d-flex align-items-center align-items-center">
+                        Save more with Bellotero.io
+                    </h4>
+                    <p className="desc_header pt-4">
+                        With Bellotero.io you save time and money make real-time decisions that boost your business and your bottom line. Get less wrongfully blocked payments, save time on bookkeeping and no need to worry about safety.
+                    </p>
+                </div>
+                <div className="calculator_section w-100"></div>
+            </div>
+        )
+    }
 }
 
 export default Configurator;
